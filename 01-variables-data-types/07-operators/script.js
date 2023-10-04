30a4ad2770c2c439c5cc437bdf9cad71f5fbdcdf
lemasani <brianlemasan@gmail.com>
2023-10-04 14:41:48 +0300

    operators

---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,39 @@
+// 1. Arithmetic operator
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// concantenation
+x = 'hello' + ' ' + 'world';
+
+// exponent
+x = 2 ** 3;
+
+//increament
+x = 1;
+x++;
+
+// decrement
+x--;
+
+// assignment operators
+x = 10; // = is the assignment
+
+// comparison operators
+x = 2 == 2; // == compares value only
+
+x = 2 === '2'; // triple === compares type and value
+
+x = 2 != 2; // != means not equal
+
+x = 10 > 5;
+x = 10 < 20;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
