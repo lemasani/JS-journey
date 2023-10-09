9ee79ca7b8630a360407f34baeece848d0340ad7
lemasani <brianlemasan@gmail.com>
2023-10-09 15:14:23 +0300

    Math object

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,22 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.32);
+
+x = Math.ceil(4.3);
+
+x = Math.floor(4.8);
+
+x = Math.pow(2, 4);
+
+x = Math.min(4, 6);
+
+x = Math.max(24, 52);
+
+x = Math.random() * 10 + 1;
+x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
