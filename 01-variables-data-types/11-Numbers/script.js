c470559d9b3743cd8e3f0cd8e87f1b45b36062ee
lemasani <brianlemasan@gmail.com>
2023-10-09 15:04:34 +0300

    numbers

---
+++ 01-variables-data-types/11-Numbers/script.js
@@ -0,0 +1,21 @@
+let x;
+
+const num = new Number(5.4245);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(3);
+
+x = num.toExponential(2);
+
+x = num.toLocaleString('en-UK');
+
+x = num.valueOf();
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+console.log(x, typeof x);
