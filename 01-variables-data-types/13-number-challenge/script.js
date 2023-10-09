02af102bc5ff73acd51171c99deb33b2e7dcd911
lemasani <brianlemasan@gmail.com>
2023-10-09 16:07:06 +0300

    numbers challenge

---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,19 @@
+let x;
+let y;
+
+x = Math.floor(Math.random() * 100 + 1);
+
+y = Math.floor(Math.random() * 50 + 1);
+
+console.log(`value of X: ${x}`);
+console.log(`value of y: ${y}`);
+
+const sumOutput = x + y;
+const difference = x - y;
+const product = x * y;
+const division = x / y;
+
+console.log(sumOutput, `x: ${x} & y: ${y}`);
+console.log(difference, `x: ${x} & y: ${y}`);
+console.log(product, `x: ${x} & y: ${y}`);
+console.log(division, `x: ${x} & y: ${y}`);
