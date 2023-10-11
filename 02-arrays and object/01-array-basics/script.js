3c3b8b59aa3db6674efb256125c4d2e29fc10df8
lemasani <brianlemasan@gmail.com>
2023-10-11 14:36:31 +0300

    array basics

---
+++ 02-arrays and object/01-array-basics/script.js
@@ -0,0 +1,21 @@
+// Array literals
+const numbers = [12, 34, 24, 53, 245];
+
+// Array constructor
+const fruits = new Array('apple', 'banana', 'grape', 'orange');
+
+x = numbers[0];
+
+x = numbers[0] + numbers[3];
+
+x = `my favorite fruit is an ${fruits[3]}`;
+
+x = numbers.length;
+
+x = fruits[2] = 'pear';
+
+fruits[fruits.length] = 'blueberry';
+
+x = fruits;
+
+console.log(x);
