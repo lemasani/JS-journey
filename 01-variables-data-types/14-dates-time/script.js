8560b7162580f68eca0914af6191b9f402c88a39
lemasani <brianlemasan@gmail.com>
2023-10-11 10:54:18 +0300

    date and time

---
+++ 01-variables-data-types/14-dates-time/script.js
@@ -0,0 +1,17 @@
+let d;
+
+d = new Date();
+
+d = d.toString();
+
+d = new Date(2021, 8, 23, 23, 54, 21);
+
+d = new Date('2021-07-23T12:42:00');
+
+// timestamps
+d = Date.now();
+
+d = new Date('2021-4-23');
+d = d.getTime();
+
+console.log(d); //typeof Object
