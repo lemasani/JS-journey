86bf30d7b0d5bda9b8e95753422d718bdf834902
lemasani <brianlemasan@gmail.com>
2023-10-26 18:14:56 -0700

    function basics

---
+++ 03-function & scope/01-function basic/script.js
@@ -0,0 +1,19 @@
+function sayHello(){
+  console.log('hello world');
+}
+
+sayHello()
+
+function add(num1, num2){
+  console.log(num1 + num2);
+}
+
+add(5,24)
+
+function substract(num1, num2){
+  return num1 - num2
+}
+
+const result = substract(20,4)
+
+console.log(result);
