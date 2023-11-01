b7625b700ce2985450398c6b15fed7fc3b985e91
lemasani <brianlemasan@gmail.com>
2023-11-01 17:24:00 +0300

    if statement

---
+++ 04-logic control/01-if statement/script.js
@@ -0,0 +1,21 @@
+// if statement
+
+if(true){
+    console.log('this is true');
+}
+
+if(false){
+    console.log('this is not true');
+}
+const x = 10
+const y = 5
+if (x >= y){
+    console.log(`${x} is greater than ${y}`);
+}
+
+if(x === y){
+    console.log(`${x} is equal to ${y}`);
+}else{
+    console.log(`${x } is not equal to ${y}`);
+}
+
