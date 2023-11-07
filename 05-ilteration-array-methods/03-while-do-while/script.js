a642d52222c3f01726cf5727b5ad728bbc29baa1
lemasani <brianlemasan@gmail.com>
2023-11-07 22:56:33 +0300

    while & do while loop

---
+++ 05-ilteration-array-methods/03-while-do-while/script.js
@@ -0,0 +1,36 @@
+// while
+
+let i =0
+// while(i <= 5){
+//     console.log('Number ' + i);
+//     i++
+// }
+
+// while loop over arrays
+
+const arr = [12,34,523,45] 
+
+// while(i < arr.length){
+//     console.log(arr[i]);
+//     i++
+// }
+
+// nesting while loops
+// while(i <= 5){
+//     console.log('Number: ' + i);
+
+//     let k = 1
+//     while(k <= 5){
+//         console.log(`${i} * ${k} = ${i * k}`);
+//         k++
+//     }
+//     i++
+// }
+
+
+// do while
+
+do{
+    console.log('Number: ' + i);
+    i++
+}while(i <= 20)
