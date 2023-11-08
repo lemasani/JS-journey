94a4a3bfdde7c86c09b31b605a780c3f40b9ba7c
lemasani <brianlemasan@gmail.com>
2023-11-08 22:22:48 +0300

    for in loop

---
+++ 05-ilteration-array-methods/06-for in loop/script.js
@@ -0,0 +1,17 @@
+const colorob = {
+    color1: 'red',
+    color2: 'green',
+    color3: 'black',
+    color4: 'white',
+    color5: 'orange'
+}
+
+for (const key in colorob){
+    console.log(key,colorob[key]);
+}
+
+const colorarr =['red', 'green', 'yellow']
+
+for(const key in colorarr){
+    console.log(colorarr[key]);
+}
