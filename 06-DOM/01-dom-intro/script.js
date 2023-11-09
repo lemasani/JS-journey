f6b35dc74db17d1cdb2c8ad6fea98ad17734dea8
lemasani <brianlemasan@gmail.com>
2023-11-09 07:38:13 +0300

    dom intro

---
+++ 06-DOM/01-dom-intro/script.js
@@ -0,0 +1,17 @@
+//console.log(window);
+// console.log(window.document);
+
+// console.log(document.body.innerHTML);
+
+//console.log(document.links);
+
+//document.body.innerHTML = '<h1> hello world</h1>'
+
+
+//document.write('hello from DOM')
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main')
+//main.innerHTML = '<h1> hello main</h1>'
+document.querySelector('#main h1').innerText = 'hello'
