46fb5fd9e7017841032df1e7e989ca3ac233dd8f
lemasani <brianlemasan@gmail.com>
2024-01-05 18:25:47 +0300

    promise.all()

---
+++ 08-asynchoronus Javascript/10-Promise.all/script.js
@@ -0,0 +1,36 @@
+function getData(endpoint){
+    return new Promise((resolve, reject)=>{
+
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', endpoint)
+        xhr.onreadystatechange = function (){
+            if(this.readyState === 4){
+                if(this.status === 200){
+                    resolve(JSON.parse(this.responseText));
+                }else{
+                    reject('something went wrong')
+                }
+            }
+        }
+    
+        setTimeout(()=>{
+            xhr.send()
+    
+        }, Math.floor(Math.random() * 3000) + 1000)
+    })
+}
+
+const moviePromise = getData('./movies.json')
+const actorsPromise = getData('./actors.json')
+const directorsPromise = getData('./directors.json')
+const dummyPromise = new Promise((resolve, reject)=>{
+    resolve('hello world')
+})
+
+Promise.all([moviePromise, actorsPromise, directorsPromise, dummyPromise])
+.then((data) =>{
+    console.log(data)
+})
+.catch((error) =>{
+    console.log(error);
+})
